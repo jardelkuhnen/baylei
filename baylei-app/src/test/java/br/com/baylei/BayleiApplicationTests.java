9fa65f4af7a563e65d9412d5773da4fd2fdb566c
jardelkuhnen <jardelkuhnen@gmail.com>
2021-06-09 22:46:22 -0300

    Startet project

---
+++ baylei-app/src/test/java/br/com/baylei/BayleiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.baylei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BayleiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
