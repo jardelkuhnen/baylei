d41042d80315f081154aad7f03e64a2c21a63f95
jardelkuhnen <jardel.kuhnen@ambevtech.com.br>
2021-10-21 21:16:07 -0300

    Changes

--- baylei-infra/baylei-adapter-h2/src/main/java/br/com/baylei/h2/config/H2Config.java
+++ baylei-infra/baylei-adapter-h2/src/main/java/br/com/baylei/h2/config/H2Config.java
@@ -1,13 +1,49 @@
 package br.com.baylei.h2.config;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration
-@EnableJpaRepositories(basePackages = {"br.com.baylei.h2"})
+@EnableTransactionManagement
+@EnableJpaRepositories(
+        entityManagerFactoryRef = "h2EntityManagerFactory",
+        transactionManagerRef = "h2TransactionManager",
+        basePackages = {"br.com.baylei.h2"})
 public class H2Config {
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.h2")
+    public DataSource getDatasource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean h2EntityManagerFactory(final EntityManagerFactoryBuilder factoryBuilder) {
+        return factoryBuilder
+                .dataSource(getDatasource())
+                .packages("br.com.baylei.h2")
+                .persistenceUnit("h2PersistenceUnit")
+                .build();
+    }
+
+    @Bean
+    public JpaTransactionManager h2TransactionManager(@Qualifier("h2EntityManagerFactory") final EntityManagerFactory factory) {
+        return new JpaTransactionManager(factory);
+    }
+
 }
--- baylei-infra/baylei-adapter-h2/src/main/java/br/com/baylei/h2/repository/ClientRepository.java
+++ baylei-infra/baylei-adapter-h2/src/main/java/br/com/baylei/h2/repository/ClientRepository.java
@@ -3,7 +3,11 @@ package br.com.baylei.h2.repository;
 import br.com.baylei.h2.entity.ClientEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository("client-repository-h2")
+@Transactional("h2TransactionManager")
 public interface ClientRepository extends JpaRepository<ClientEntity, String> {
+
+
 }
